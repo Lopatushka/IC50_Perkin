1779d0789da80709500ef89742dd89fcbbff620c
Elena Lopatukhina <helena.lopatukhina@gmail.com>
2021-11-25 16:16:07 +0300

    download row data

--- 01.04.19_MTT_Perkin_script.R
+++ 01.04.19_MTT_Perkin_script.R
@@ -4,16 +4,22 @@ cell_line_names <- "HEK293"
 
 # Download functions
 source("functions.R")
+todor::todor()
 
 # Paths to files
 working_dir <- getwd()
-
 path_data <- list.files(path = name_of_dir, ignore.case=TRUE,
                         full.names = TRUE,pattern = cell_line_names)
 path_names <- list.files(path = name_of_dir, ignore.case=TRUE,
                          full.names = TRUE,pattern = "names")
 path_conc <- list.files(path = name_of_dir, ignore.case=TRUE,
                         full.names = TRUE,pattern = "concentrations")
-# Create new dir!!!!
+
+# TODO Create a dir
 path_export <- paste(name_of_dir, "results", sep="/")
 
+# Download row data
+data <- ImportDataFile(path_data)
+data <- AddDrugNames(data, path_names)
+data <- AddConcentrations(df = data, path_conc = path_conc)
+data <- DropNull(data)

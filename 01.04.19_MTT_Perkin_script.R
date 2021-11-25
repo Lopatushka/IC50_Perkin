e1ad0aeee94f243b40d70df0999c252236309bcd
Elena Lopatukhina <helena.lopatukhina@gmail.com>
2021-11-25 14:01:30 +0300

    delete dir. create paths

--- 01.04.19_MTT_Perkin_script.R
+++ 01.04.19_MTT_Perkin_script.R
@@ -1,2 +1,19 @@
-# Download fuctions
+# Enter data
+name_of_dir <- "01.04.19_MTT_Perkin_data"
+cell_line_names <- "HEK293"
+
+# Download functions
 source("functions.R")
+
+# Paths to files
+working_dir <- getwd()
+
+path_data <- list.files(path = name_of_dir, ignore.case=TRUE,
+                        full.names = TRUE,pattern = cell_line_names)
+path_names <- list.files(path = name_of_dir, ignore.case=TRUE,
+                         full.names = TRUE,pattern = "names")
+path_conc <- list.files(path = name_of_dir, ignore.case=TRUE,
+                        full.names = TRUE,pattern = "concentrations")
+# Create new dir!!!!
+path_export <- paste(name_of_dir, "results", sep="/")
+
